62f54cef4e441c5e6dd79d59def07da670283cbf
Lleyton Gray <lleyton@fyralabs.com>
2023-11-12 10:40:52 -0800

    Minimally customize theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Fyra Devdocs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/FyraLabs/devdocs-next",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.gg/BHNfGewTXX",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/FyraLabs/devdocs-next",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "MIT 2023 © Fyra Labs; Made with love by catgirls",
   },
-}
+};
 
-export default config
+export default config;
