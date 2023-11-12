a11704d59d213ec669bb6f386969b7584e07f1a3
Lleyton Gray <lleyton@fyralabs.com>
2023-11-12 10:11:59 -0800

    Import content from old docs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const { BUNDLED_LANGUAGES, getHighlighter } = require("shiki");
 
-module.exports = withNextra()
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  mdxOptions: {
+    rehypePrettyCodeOptions: {
+      getHighlighter: (options) =>
+        getHighlighter({
+          ...options,
+          langs: [
+            ...BUNDLED_LANGUAGES,
+            // From https://github.com/1dot75cm/vscode-rpm-spec, thanks!
+            {
+              id: "spec",
+              scopeName: "source.spec",
+              aliases: ["rpmspec"],
+              path: "../../public/spec.json",
+            },
+          ],
+        }),
+    },
+  },
+});
+
+module.exports = withNextra();
