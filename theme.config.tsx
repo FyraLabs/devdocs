5e743ad63144e4a113588b960431208f794d29a5
Lleyton Gray <lleyton@fyralabs.com>
2023-11-12 12:24:09 -0800

    Adjust hue again

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  primaryHue: 270,
+  primaryHue: 287,
   logo: (
     <>
       <svg
