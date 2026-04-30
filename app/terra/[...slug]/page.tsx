76de343b004ef38ff08c9e443a844185fce11dad
Gilver <roachy@fyralabs.com>
2026-04-29 23:01:31 -0500

    feat: Cursed slug redirects

--- app/terra/[...slug]/page.tsx
+++ app/terra/[...slug]/page.tsx
@@ -1,9 +1,43 @@
 import { permanentRedirect } from "next/navigation";
 
-export function generateStaticParams() {
-    return [{ slug: ["redirect"] }]
+export async function generateStaticParams() {
+  return [
+    { slug: ["appstream"] },
+    { slug: ["autoupdate"] },
+    { slug: ["contributing"] },
+    { slug: ["faq"] },
+    { slug: ["guidelines"] },
+    { slug: ["index"] },
+    { slug: ["infrastructure"] },
+    { slug: ["installing"] },
+    { slug: ["lifecycle"] },
+    { slug: ["policies"] },
+    { slug: ["srpm"] },
+  ];
 }
 
-export default async function Page() {
-    permanentRedirect("https://docs.terrapkg.com");
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
+
+  let url = "";
+  if (
+    slug.toString() === "appstream" ||
+    slug.toString() === "autoupdate" ||
+    slug.toString() === "contributing" ||
+    slug.toString() === "guidelines" ||
+    slug.toString() === "policies" ||
+    slug.toString() === "srpm"
+  ) {
+    url = `https://docs.terrapkg.com/contributing/${slug}`;
+  } else if (slug.toString() === "installing" || slug.toString() === "lifecycle") {
+    url = `https://docs.terrapkg.com/usage/${slug}`;
+  } else if (slug.toString() === "infrastructure") {
+    url = `https://docs.terrapkg.com/general/${slug}`;
+  } else if (slug.toString() === "faq") {
+    url = `https://docs.terrapkg.com/reference/${slug}`;
+  } else {
+    url = "https://docs.terrapkg.com";
+  }
+
+  permanentRedirect(url);
 }
